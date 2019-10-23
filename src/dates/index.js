de7f7d19cf7232f47a447503931e898e87ce31ff
amberstyers <amberstyers@gmail.com>
2019-10-23 16:36:02 -0500

    5 passed

--- src/dates/index.js
+++ src/dates/index.js
@@ -1,19 +1,26 @@
 // import moment here; use this package in each function
+const moment = require("moment");
 
 const today = () => {
-  // write code for dates.today
+  const day = moment().format("dddd");
+  console.log("Today is:",day);
+  return day;
 
-}
+};
 
 const calendar = () => {
-  // write code for dates.calendar
+  const date = moment().format("LL");
+  console.log("Today's date:", date);
+  return date;
 
-}
+};
 
 const currentTime = () => {
-  // write code for dates.currentTime
+  const time = moment().format('hLTS')
+  console.log("The time is:", time)
+  return time;
 
-}
+};
 
 module.exports = {
   today,
--- src/strings/index.js
+++ src/strings/index.js
@@ -1,10 +1,18 @@
 const split = (str, delim) => {
-  // write code for strings.split
-
+  return str.split(delim);
 }
 
-const pairs = (str) => {
-  // write code for strings.pairs
+const pairs = () => {
+  // let strArr = Array.from(str)
+  // let pairs = []
+  // for (let i = 0; i< strArr.length; i+=2) {
+  //   let element =strArr[i]
+  //   let chunks = [element, strArr[i + 1]]
+  //   let joinedChunks = chunks.join('')
+  //   console.log(joinedChunks)
+  //   pairs.push(joinedChunks)
+  // }
+  // return pairs 
 
 }
 
